5fbd8b6df194097398003e3667e9f5b0fe713a10
Aapo Korhonen <aapo.korhonen23@gmail.com>
2022-03-10 20:33:51 +0200

    Database ja game työstetty

--- RPC_Server/database.py
+++ RPC_Server/database.py
@@ -1,16 +1,65 @@
+import sqlite3
+import threading
+
+
 class Database:
     
     
-    def __init__():
-        "Initializing database"
+    def __init__(self ,filename):
+    
+        self.conn = sqlite3.connect(filename)
+        self.id = 0
+        self.make_tables()
+        self._lock = threading.Lock()
+        
+        
+        
+    
+    
+    def make_tables(self):
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute("create table USERS (USER STRING PRIMARY KEY,PASSWORD STRING NOT NULL) " )
+            cursor.execute("create table GAMES (ID INT PRIMARY KEY, PLAYER1 STRING NOT NULL, PLAYER2 STRING NOT NULL, OUTCOME STRING) " )
+            self.conn.commit()
+
+        except:
+            print("Taulukost jo olemassa")
+            self.id = self.init_id()
+            
+
+    def init_id(self):
+        
+        try:
+            cursor = self.connect.cursor()
+            cursor.execute("SELECT ID FROM GAMES WHERE ID IS (SELECT MAX(ID) FROM GAMES)")
+            ID = int(cursor.fetchall())
+            print("Onnistui")
+        except:
+            print("ERRORi")
+            ID = 0
+        
+        return ID
+     
+    def get_ID(self):
+    
+        with self._lock:
+            ID = self.id
+            self.id = self.id + 1
+            
+        return ID
+
+     
+    def insert_new_user(self, username, password):
+    
+        cursor = self.conn.cursor()
+        
+    
+    
         return 0
         
     
-    def insert_new_user():
-        return 0
-        
-    
-    def insert_game_history():
+    def insert_game_history(self, ID , p1, p2, outcome):
         return 0
         
     
@@ -20,3 +69,12 @@ class Database:
     
     def get_game_information():
         return 0
+        
+    def get_game_info_user():
+        return 0
+    
+    def get_game_info_wins_user():
+        return 0
+        
+        
+        
--- RPC_Server/game.py
+++ RPC_Server/game.py
@@ -1,2 +1,93 @@
+
+
 class Game:
+
+    def __init__(self, p1, p2):
+        
+        self.p1 = p1
+        self.p2 = p2
+        self.p1move = None
+        self.p2move = None
+        self.outcome = None
+        
+    
+    def p1_move(self, move):
+        self.p1move = move
+    
+    def p2_move(self, move):
+        self.p2move = move
+
+    def solve_game(self, ):
+        
+        if self.p1move == "R":
+            if self.p2move == "R":
+                self.outcome = "Tie"
+            elif self.p2move == "P":
+                self.outcome = self.p2
+            elif self.p2move == "S":
+                self.outcome = self.p1
+                        
+        elif self.p1move == "P":
+                
+            if self.p2move == "R":
+                self.outcome = self.p1
+            elif self.p2move == "P":
+                self.outcome = "Tie"
+            elif self.p2move == "S":
+                self.outcome = self.p2
+
+        elif self.p1move == "S":
+        
+            if self.p2move == "R":
+                self.outcome = self.p2
+            elif self.p2move == "P":
+                self.outcome = self.p1
+            elif self.p2move == "S":
+                self.outcome = "Tie"
+
+        
+        return self.outcome
+        
+    
     pass
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ #       match self.p1move:
+ #           case "R":
+ #           
+ #               match self.p2move:
+ #                   case "R":
+ #                       self.outcome = "Tie"
+ #                   case "P":
+ #                      self.outcome = self.p2
+ #                  case "S":
+  #                      self.outcome = self.p1
+
+ #           case "P":
+ #               match self.p2move:
+ #                   case "R":
+ #                       self.outcome = self.p1
+ #                   case "P":
+ #                       self.outcome = "Tie"
+ #                   case "S":
+ #                       self.outcome = self.p2
+
+ #           case "S":
+ #               match self.p2move
+ #                   case "R":
+ #                       self.outcome = self.p2
+ #                   case "P":
+ #                       self.outcome = self.p1
+ #               
+ #                   case "S":
+ #                       self.outcome = "Tie"
+#
