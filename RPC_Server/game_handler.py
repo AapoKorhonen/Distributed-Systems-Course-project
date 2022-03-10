92307a8babd8f7c74a95724d916e32acc033d6e4
PatsiSus <susanna.patsi@outlook.com>
2022-03-10 20:39:06 +0200

    game_hander ja game_generator def added

---
+++ RPC_Server/game_handler.py
@@ -0,0 +1,26 @@
+import game
+
+
+class GameHandler:
+
+    def __init__(self, database):
+        self.db = database
+        self.player1 = None
+        self.player2 = None
+
+    def alusta_pelaajat(self, p1 =None, p2=None):
+        if not self.player1:
+            self.player1 = p1
+
+        else if not self.player2:
+            self.player2 = p2
+
+        
+
+    def create_db_object(self, p1, p2, outcome):
+        #kutsuu databasea ja luo sinne uuden objektin
+        __gameID = self.db.----#1.luo database funktiolla uuden ID
+        self.db.-----(gameID, p1, p2, outcome) #luo uuden objektin
+
+
+    
--- RPC_Server/rpc_server.py
+++ RPC_Server/rpc_server.py
@@ -10,7 +10,7 @@ import datetime
 import socket
 import ssl
 import error_handler
-
+import game_generator
 
 class RPCServer:
     def __init__(self, database, pem, key):
@@ -32,7 +32,9 @@ class RPCServer:
                     # Keep accepting connections from clients
                     with context.wrap_socket(sock, server_side=True) as ssock:
                         # (clientConnection, clientAddress) = serverSocket.accept()
-                        conn, addr = ssock.accept()
+                        print("kakkapaa")
+                        conn, addr = ssock.accept() #Tässä on homma
+                        print("pylly")
                         print(conn.getpeercert)
                         # Send current server time to the client
                         serverTimeNow = "%s"%datetime.datetime.now()
@@ -88,6 +90,7 @@ class RPCServer:
         server.log("Creating socket connection...")
         server._create_socket()
         server.log("Creating game handler...")
+       
         
 
 
